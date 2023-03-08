d3a5b332c6df6b0240aa99b60c157b2b28f18736
Arjun <arjunvs.vs@gmail.com>
2023-03-08 12:51:50 +0530

    Removed Commented code

--- downloader_cli/cli.py
+++ downloader_cli/cli.py
@@ -10,30 +10,6 @@ import inquirer
 console = Console()
 
 
-# def start_cli():
-#     print(pyfiglet.figlet_format("CWM - D", font="5lineoblique"))
-#
-#
-#     user_input = show_prompt(
-#         options=[
-#             "Download a single video from URL",
-#             "Download all videos from a course",
-#             "Exit",
-#         ]
-#     )
-#     if user_input == 0:
-#         download_single_lecture_video()
-#     elif user_input == 1:
-#         download_multiple_lecture_videos()
-#     elif user_input == 2:
-#         exit()
-#     else:
-#         console.print(
-#             "Invalid Input/Error Processing Input",
-#             style=Style(color="red", blink=False, bold=True),
-#         )
-
-
 def start_cli():
     print(pyfiglet.figlet_format("CWM - D", font="5lineoblique"))
 
@@ -53,16 +29,9 @@ def start_cli():
         exit()
 
 
-# def show_prompt(options):
-#     terminal_menu = TerminalMenu(options)
-#     menu_entry_index = terminal_menu.show()
-#     return menu_entry_index
-
-
 def show_inquirer_prompt(message, choices):
     questions = [
         inquirer.List("user_input", message=message, choices=choices, carousel=True),
     ]
     answers = inquirer.prompt(questions)
-    return answers.get('user_input')
-
+    return answers.get("user_input")
